084dfc10215176d1edbf262744c96c9d5c915c56
asoordelu3 <asoordel@purdue.edu>
2020-04-15 22:36:06 -0700

    Return list of quotes and cache quotes

--- routes/routes.py
+++ routes/routes.py
@@ -6,6 +6,9 @@ import string
 app = Flask(__name__)
 CORS(app)
 
+quotesCache = []
+cacheIndex = 0
+
 @app.route("/query/titleOrAuthorSearch/<string:queryParameter>", methods=["GET"])
 def titleOrAuthorSearch(queryParameter):
     #TODO DB call
@@ -16,9 +19,15 @@ def titleOrAuthorSearch(queryParameter):
 def tagSearch(tags):
     #TODO DB call
     code = 200
-    quote = ''.join(random.choices(string.ascii_letters + string.digits, k = random.randint(500,3500)))
-    return json.dumps({"quote" : quote, "Title": "TestTitle", "Author" : "TestAuthor",
-                       "tags" : [tags]}), code, {"ContentType": "application/json"}
+    global quotesCache
+    global cacheIndex
+    if (len(quotesCache) == 0): #Populate cache
+        quotes = [''.join(random.choices(string.ascii_letters + string.digits, k = random.randint(500,3500))) for x in range(100)]
+        quotesCache = [(json.dumps({"quote" : quotes[i], "Title": "TestTitle", "Author" : "TestAuthor",
+                           "tags" : [tags]}), code, {"ContentType": "application/json"}) for i in range(100)]
+    quotesToReturn = json.dumps({"quotes": quotesCache[cacheIndex: cacheIndex + 10]}) #TODO indexing past end of array
+    cacheIndex += 10
+    return quotesToReturn
 
 @app.route("/query/getTags/", methods=["GET"])
 def getTags():
